3ff80b6f9580b065baa4ab35117f923a73980764
PedroM <03314418027@senacrs.edu.br>
2025-09-17 10:13:53 -0300

    entrega

--- projetos/atividade entrega/entrega.js
+++ projetos/atividade entrega/entrega.js
@@ -1,16 +1,23 @@
-let cliente = "Cliente X";
-let endereco = "Rua Dedão do Pé, 124";
-let distanciaKm = 30;
+let cliente = prompt("Digite o nome do cliente:");
+let endereco = prompt("Digite o endereço do cliente:");
+let distanciaKm = parseFloat(prompt("Digite a distância em km:"));
 
 let tempoHrs = distanciaKm / 40;
 let tempoMin = tempoHrs * 60;
 
 console.log(tempoMin);
 
+let classificacao;
+
 if (tempoMin <= 30) {
-  console.log("Rápida");
+  classificacao = "Rápida";
 } else if (tempoMin > 30 && tempoMin <= 120) {
-  console.log("Média");
+  classificacao = "Média";
 } else {
-  console.log("Demorada");
+  classificacao = "Demorada";
 }
+
+console.log(classificacao);
+
+// Alert final
+alert(`Cliente: ${cliente}\nEndereço: ${endereco}\nTempo estimado: ${tempoMin.toFixed(2)} minutos\nClassificação: ${classificacao}`);
